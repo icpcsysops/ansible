08c06d6ce02a704cb1081678c4a7d1caaa4eb6e6
Troy Boudreau <tboudreau@us.ntt.net>
2024-04-14 11:37:06 +0200

    ip link output seems to have changed in 22.04

--- do.grab.ethers.sh
+++ do.grab.ethers.sh
@@ -2,4 +2,5 @@
 # wf2023 value
 image_number=6
 ansible-playbook --limit contestants ip_link_show.yml > ip_link.output.txt
-grep -B3 ether ip_link.output.txt  |egrep '(ok|ether)'  | sed 'N;s/\n/ /' | awk '{printf "%s,%s,,,,$image_number\n", $7, $2}' | tr -d \]\[ > ether.list.txt
+#ansible-playbook --limit team4600* ip_link_show.yml > ip_link.output.txt
+grep -B3 ether ip_link.output.txt  |egrep '(ok|ether)'  | sed 'N;s/\n/ /' | awk '{printf "%s,%s,,,,$image_number\n", $8, $2}' | tr -d \]\[ > ether.list.txt

5d1bddd32acbbdadf6028e2c526882e7a68d218d
Troy Boudreau <troy@icpc.global>
2025-09-03 13:18:06 +0400

    use fast ansible

--- reimage.sh
+++ reimage.sh
@@ -4,6 +4,6 @@ echo "TARGET of reimaging is \"$TARGET\""
 echo "Continue?  (yes/NO)"
 read key
 if [ "$key" == "yes" ]; then
-    ansible -m shell -a "efibootmgr -n \$(efibootmgr | grep -E 'IPv4|Network Device' | head -n1 | awk '{print \$1}' | tr -d '[A-z*]')" "$TARGET"
-    ansible -m shell -a reboot "$TARGET"
+    ./fast-ansible -m shell -a "efibootmgr -n \$(efibootmgr | grep -E 'PXE BOOT|IPv4|Network Device' | head -n1 | awk '{print \$1}' | tr -d '[A-z*]')" "$TARGET"
+    ./fast-ansible -m shell -a reboot "$TARGET"
 fi

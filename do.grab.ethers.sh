7dbf64f043be73edcacba3cdb79e82e89ba4bffc
NAC SysOps <icpcsysops@icpc.global>
2026-03-20 11:48:27 -0400

    make the image_id variable

--- do.grab.ethers.sh
+++ do.grab.ethers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# image_id, defaults to 1 if not given on the commandline below refers to the image number on the fog server
+image_id=${1:-1}
 ansible-playbook --limit contestants ip_link_show.yml > ip_link.output.txt
-# 10 below refers to the image number on the fog server
-grep -B3 ether ip_link.output.txt  |egrep '(ok|ether)'  | sed 'N;s/\n/ /' | awk '{printf "%s,%s,,,10\n", $8, $2}' | tr -d \]\[ > ether.list.csv
+grep -B3 ether ip_link.output.txt  |egrep '(ok|ether)'  | sed 'N;s/\n/ /' | awk "{printf \"%s,%s,,,$image_id\n\", \$8, \$2}" | tr -d \]\[ > ether.list.csv
